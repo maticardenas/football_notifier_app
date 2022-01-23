3091e3b9acd77c8e9fef893c213fc100eeec9f6c
Matias Cardenas <matias.cardenas@sendcloud.com>
2022-01-23 13:07:04 +0100

    feat: models migrations and pytest compliance

--- app/notifier/models.py
+++ app/notifier/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from app import settings
+
 
 class Tournament(models.Model):
     tour_id = models.CharField(max_length=255)
@@ -15,3 +17,34 @@ class Team(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Notification(models.Model):
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
+    )
+    league = models.ForeignKey(
+        Tournament,
+        on_delete=models.CASCADE,
+    )
+    season = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.team.name} - {self.league.name} - {self.season}"
+
+
+class NotifSubscription(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    notif = models.ForeignKey(
+        Notification,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.notif)
+
+
--- app/notifier/tests.py
+++ app/notifier/tests.py
@@ -1,23 +1,48 @@
+import pytest
 from django.test import TestCase
 from notifier import models
 
 
+@pytest.fixture(autouse=True)
+def db(request, django_db_setup, django_db_blocker):
+    django_db_blocker.unblock()
+
 
 def test_team_model_representation():
     team_name = "River Plate"
-    model = models.Team.create(
-        id="2",
+    model = models.Team.objects.create(
+        team_id="2",
         name=team_name
     )
 
     assert str(model) == team_name
 
 
-def test_tournament_model_repreentation():
+def test_tournament_model_representation():
     tour_name = "Liga Profesional de Fútbol"
-    model = models.Tournament.create(
-        id="2",
+    model = models.Tournament.objects.create(
+        tour_id="2",
         name=tour_name
     )
 
     assert str(model) == tour_name
+
+def test_notif_representation():
+    team_name = "River Plate"
+    tour_name = "Liga Profesional de Fútbol"
+    team = models.Team.objects.create(
+        team_id="2",
+        name=team_name
+    )
+    tour = models.Tournament.objects.create(
+        tour_id="2",
+        name=tour_name
+    )
+
+    notif = models.Notification.objects.create(
+        team=team,
+        league=tour,
+        season="2021"
+    )
+
+    assert str(notif) == f"{team_name} - {tour_name} - 2021"
